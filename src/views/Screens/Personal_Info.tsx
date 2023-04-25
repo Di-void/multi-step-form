cce7aafd2177b5f618ee0b4d46ddd4319b26b41f
DEE <donzydonathan@gmail.com>
2023-04-25 17:42:43 +0100

    feat: complete implementing personal info form fields design

--- src/views/Screens/Personal_Info.tsx
+++ src/views/Screens/Personal_Info.tsx
@@ -1,3 +1,5 @@
+import InfoForm from "@/components/Forms/InfoForm";
+
 const PersonalInfo = () => {
   return (
     <div>
@@ -5,9 +7,11 @@ const PersonalInfo = () => {
         <h1 className="text-marine-blue text-2xl font-bold">Personal info</h1>
       </header>
 
-      <p className="text-cool-gray text-lg">
+      <p className="text-cool-gray text-lg mb-4">
         Please provide your name, email address and phone number.
       </p>
+
+      <InfoForm />
     </div>
   );
 };
--- src/views/index.tsx
+++ src/views/index.tsx
@@ -2,7 +2,7 @@ import { PersonalInfo } from "./Screens";
 
 const Views = () => {
   return (
-    <section className="bg-white shadow-xl rounded-lg mt-7 xl:mt-0 px-4 py-6 lg:px-6 lg:py-8">
+    <section className="bg-white shadow-xl rounded-lg mt-7 xl:mt-0 px-5 py-6 lg:px-6 lg:py-8">
       <PersonalInfo />
     </section>
   );
