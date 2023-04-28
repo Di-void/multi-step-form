c3d7a52b3509500ca6bcf831d4cead8196f02df5
DEE <donzydonathan@gmail.com>
2023-04-28 14:04:02 +0100

    (refactor,fix): clean up switch component and fix bugs in icons components

--- src/components/Switch/index.tsx
+++ src/components/Switch/index.tsx
@@ -1,35 +1,16 @@
 import { useState } from "react";
 
 const Switch = () => {
-  //   const [enabled, setEnabled] = useState(false);
   const [toggle, setToggle] = useState(false);
-  const toggleClass = " transform translate-x-5";
 
   return (
-    // <div className="relative flex flex-col items-center justify-center min-h-screen overflow-hidden">
-    //   <div className="flex">
-    //     <label className="inline-flex relative items-center mr-5 cursor-pointer">
-    //       <input
-    //         type="checkbox"
-    //         className="sr-only peer"
-    //         checked={enabled}
-    //         readOnly
-    //       />
-    //       <div
-    //         onClick={() => {
-    //           setEnabled(!enabled);
-    //         }}
-    //         className="w-11 h-6 bg-gray-200 rounded-full peer  peer-focus:ring-green-300  peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green-600"
-    //       ></div>
-    //     </label>
-    //   </div>
-    // </div>
     <div className="h-fit">
       <input
         type="checkbox"
         id="toggle"
         className="hidden peer"
         checked={toggle}
+        readOnly
       />
       <label
         className="md:w-12 md:h-7 w-10 h-5 flex items-center bg-marine-blue rounded-full p-1 cursor-pointer"
