9331c1a099c7f21ff62d72054f3e410c993543c7
DEE <donzydonathan@gmail.com>
2023-05-02 20:35:10 +0100

    feat: complete desktop view for finishup screen

--- src/views/Screens/FinishUp.tsx
+++ src/views/Screens/FinishUp.tsx
@@ -4,10 +4,12 @@ const FinishUp = () => {
   return (
     <div>
       <header className="mb-2">
-        <h1 className="text-marine-blue text-2xl font-bold">Finishing up</h1>
+        <h1 className="text-marine-blue text-2xl xl:text-3xl font-bold">
+          Finishing up
+        </h1>
       </header>
 
-      <p className="text-cool-gray text-lg mb-4">
+      <p className="text-cool-gray text-lg mb-4 xl:mb-7">
         Double-check everything looks OK before confirming.
       </p>
 
