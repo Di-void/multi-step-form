7a7d368769a2049e727a3dc1ded789b2f72b89d9
DEE <donzydonathan@gmail.com>
2023-05-19 19:48:38 +0100

    fix: switch back styles in desktop nav

--- src/components/Navigation/desktop/index.tsx
+++ src/components/Navigation/desktop/index.tsx
@@ -26,7 +26,7 @@ const DesktopNav = ({ page }: Page) => {
                   className={`border ${
                     page >= index
                       ? "text-black bg-light-blue border-transparent"
-                      : "border-white"
+                      : "border-white text-white"
                   } font-bold rounded-full p-4 w-3 h-3 flex items-center justify-center`}
                 >
                   <span>{index + 1}</span>
