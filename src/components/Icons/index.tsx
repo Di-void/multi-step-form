23a5bcc06865efe925c3cd791b92aff724edcf6b
DEE <donzydonathan@gmail.com>
2023-05-19 20:23:00 +0100

    build: fix strange error failing build

--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -1,7 +1,7 @@
 import Arcade from "./Arcade";
 import Pro from "./Pro";
 import Advanced from "./Advanced";
-import ThankYou from "./Thankyou";
+import ThankYouIcon from "./ThankYouIcon";
 import CheckMark from "./CheckMark";
 
-export { Arcade, Pro, Advanced, ThankYou, CheckMark };
+export { Arcade, Pro, Advanced, ThankYouIcon as ThankYou, CheckMark };
